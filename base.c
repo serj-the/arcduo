976095608edb48bb13603a9e4f006c30c59c10b9
serj-the <44114978+serj-the@users.noreply.github.com>
2024-10-26 19:19:49 +0300

    Update base.c
    предыдущий был случайным))

--- base.c
+++ base.c
@@ -3,6 +3,17 @@
 #define RELAY_3_PIN 12      // Пин для реле 3
 #define RELAY_4_PIN 13      // Пин для реле 4
 
+// Пины для монетоприемника и светодиодов
+const int coinSlotPin = 11;       // Пин монетоприемника
+const int ledCoinPin = 9;         // Пин светодиода для монетки (Coin)
+const int ledPlayer1Pin = 8;      // Пин светодиода для 1 игрока
+const int ledPlayer2Pin = 10;     // Пин светодиода для 2 игрока
+const int relayPlayer1Pin = 6;    // Пин реле для 1 игрока
+const int relayPlayer2Pin = 5;    // Пин реле для 2 игрока
+const int relayCoinPin = 4;       // Пин реле для монетки
+
+int coinCount = 0;  // Счетчик вставленных монет
+
 void setup() {
   // Настраиваем пины реле как выходы
   pinMode(RELAY_1_PIN, OUTPUT);
@@ -10,16 +21,27 @@ void setup() {
   pinMode(RELAY_3_PIN, OUTPUT);
   pinMode(RELAY_4_PIN, OUTPUT);
 
-  // Устанавливаем реле в исходное состояние (выключено)
+  // Настраиваем остальные пины как выходы
+  pinMode(coinSlotPin, INPUT_PULLUP);
+  pinMode(ledCoinPin, OUTPUT);
+  pinMode(ledPlayer1Pin, OUTPUT);
+  pinMode(ledPlayer2Pin, OUTPUT);
+  pinMode(relayPlayer1Pin, OUTPUT);
+  pinMode(relayPlayer2Pin, OUTPUT);
+  pinMode(relayCoinPin, OUTPUT);
+
+  // Инициализация реле в выключенном состоянии
   digitalWrite(RELAY_1_PIN, HIGH);
   digitalWrite(RELAY_2_PIN, HIGH);
   digitalWrite(RELAY_3_PIN, HIGH);
   digitalWrite(RELAY_4_PIN, HIGH);
+  digitalWrite(relayPlayer1Pin, HIGH);
+  digitalWrite(relayPlayer2Pin, HIGH);
+  digitalWrite(relayCoinPin, HIGH);
 
-  // Начальная задержка в 2 секунды
+  // Задержка перед включением реле поочередно
   delay(2000);
 
-  // Включаем реле одно за другим с задержкой в 1 секунду
   digitalWrite(RELAY_1_PIN, LOW);  // Включаем реле 1
   delay(1000);
   digitalWrite(RELAY_2_PIN, LOW);  // Включаем реле 2
@@ -30,5 +52,41 @@ void setup() {
 }
 
 void loop() {
-  // Основной цикл остается пустым, так как реле включаются только при старте
+  // Проверка на вставку монеты
+  if (digitalRead(coinSlotPin) == LOW) {
+    delay(50); // Антидребезг
+    if (digitalRead(coinSlotPin) == LOW) {
+      handleCoinInsertion();  // Обработка вставки монеты
+    }
+  }
+}
+
+// Обработка вставки монеты
+void handleCoinInsertion() {
+  coinCount++;
+
+  if (coinCount == 1) {
+    activateRelayAndLed(relayPlayer1Pin, ledPlayer1Pin);  // Активируем кнопку 1 игрока
+  } 
+  else if (coinCount == 2) {
+    activateRelayAndLed(relayCoinPin, ledCoinPin);        // Активируем кнопку Coin
+    activateRelayAndLed(relayPlayer2Pin, ledPlayer2Pin);  // Активируем кнопку 2 игрока
+  }
+}
+
+// Активация реле и светодиодов
+void activateRelayAndLed(int relayPin, int ledPin) {
+  digitalWrite(relayPin, LOW);  // Включаем реле
+  digitalWrite(ledPin, HIGH);   // Включаем светодиод
+}
+
+// Сброс системы в начальное состояние
+void resetSystem() {
+  coinCount = 0;
+  digitalWrite(relayPlayer1Pin, HIGH);
+  digitalWrite(relayPlayer2Pin, HIGH);
+  digitalWrite(relayCoinPin, HIGH);
+  digitalWrite(ledCoinPin, LOW);
+  digitalWrite(ledPlayer1Pin, LOW);
+  digitalWrite(ledPlayer2Pin, LOW);
 }
