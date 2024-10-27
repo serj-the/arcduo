7363c10b1d34d80f0b6b191feea11df52dea0d81
serj-the <44114978+serj-the@users.noreply.github.com>
2024-10-27 21:28:02 +0300

    Update base.c
    Вернул часть кода, убрал другую

--- base.c
+++ base.c
@@ -12,7 +12,17 @@ const int relayPlayer1Pin = 6;    // Пин реле для 1 игрока
 const int relayPlayer2Pin = 5;    // Пин реле для 2 игрока
 const int relayCoinPin = 4;       // Пин реле для монетки
 
-int coinCount = 0;  // Счетчик вставленных монет
+unsigned long timerStart = 0;            // Время начала отсчета после вставки монеты
+const unsigned long activeTime = 300000; // 5 минут (300000 миллисекунд)
+const unsigned long warningTime = 20000; // 20 секунд (20000 миллисекунд)
+unsigned long lastBreathUpdate = 0;
+
+bool gameActive = false;           // Флаг для активной игры
+bool blinkPhase = false;           // Флаг для управления миганием в последние 20 секунд
+unsigned long blinkStartTime = 0;  // Время начала мигания
+int coinCount = 0;                 // Счетчик вставленных монет
+int breathValue = 0;
+bool breathUp = true;
 
 void setup() {
   // Настраиваем пины реле как выходы
@@ -21,7 +31,10 @@ void setup() {
   pinMode(RELAY_3_PIN, OUTPUT);
   pinMode(RELAY_4_PIN, OUTPUT);
 
-  // Настраиваем остальные пины как выходы
+  // Включаем реле при старте
+  activateRelays();
+
+  // Настраиваем остальные пины
   pinMode(coinSlotPin, INPUT_PULLUP);
   pinMode(ledCoinPin, OUTPUT);
   pinMode(ledPlayer1Pin, OUTPUT);
@@ -30,48 +43,76 @@ void setup() {
   pinMode(relayPlayer2Pin, OUTPUT);
   pinMode(relayCoinPin, OUTPUT);
 
-  // Инициализация реле в выключенном состоянии
-  digitalWrite(RELAY_1_PIN, HIGH);
-  digitalWrite(RELAY_2_PIN, HIGH);
-  digitalWrite(RELAY_3_PIN, HIGH);
-  digitalWrite(RELAY_4_PIN, HIGH);
-  digitalWrite(relayPlayer1Pin, HIGH);
-  digitalWrite(relayPlayer2Pin, HIGH);
-  digitalWrite(relayCoinPin, HIGH);
-
-  // Задержка перед включением реле поочередно
-  delay(2000);
-
-  digitalWrite(RELAY_1_PIN, LOW);  // Включаем реле 1
-  delay(1000);
-  digitalWrite(RELAY_2_PIN, LOW);  // Включаем реле 2
-  delay(1000);
-  digitalWrite(RELAY_3_PIN, LOW);  // Включаем реле 3
-  delay(1000);
-  digitalWrite(RELAY_4_PIN, LOW);  // Включаем реле 4
+  resetSystem(); // Сбрасываем систему в начальное состояние
 }
 
 void loop() {
+  // Анимация светодиода, если игра не активна
+  if (!gameActive) {
+    breatheLed(ledCoinPin);
+  }
+
   // Проверка на вставку монеты
   if (digitalRead(coinSlotPin) == LOW) {
     delay(50); // Антидребезг
     if (digitalRead(coinSlotPin) == LOW) {
-      handleCoinInsertion();  // Обработка вставки монеты
+      handleCoinInsertion(); // Обрабатываем вставку монетки
+    }
+  }
+
+  if (gameActive) {
+    unsigned long currentTime = millis();
+
+    // Если прошло больше 5 минут, сбрасываем систему
+    if (currentTime - timerStart > activeTime) {
+      resetSystem();
+    }
+    // Если осталось меньше 20 секунд, мигаем светодиодом монетки
+    else if (currentTime - timerStart > (activeTime - warningTime)) {
+      blinkCoinWithIncreasingSpeed(currentTime);
     }
   }
 }
 
-// Обработка вставки монеты
+// Активация реле в начале программы
+void activateRelays() {
+  delay(10000);
+  digitalWrite(RELAY_1_PIN, LOW);
+  delay(1000);
+  digitalWrite(RELAY_2_PIN, LOW);
+  delay(2000);
+  digitalWrite(RELAY_3_PIN, LOW);  
+  delay(1000);
+  digitalWrite(RELAY_4_PIN, LOW);
+}
+
+// Обработка вставки монетки
 void handleCoinInsertion() {
   coinCount++;
+  gameActive = true;
+
+  blinkCoinThreeTimes(); // Моргание индикатора монетки три раза
 
   if (coinCount == 1) {
-    activateRelayAndLed(relayPlayer1Pin, ledPlayer1Pin);  // Активируем кнопку 1 игрока
+    activateRelayAndLed(relayPlayer1Pin, ledPlayer1Pin);
   } 
-  else if (coinCount == 2) {
-    activateRelayAndLed(relayCoinPin, ledCoinPin);        // Активируем кнопку Coin
-    activateRelayAndLed(relayPlayer2Pin, ledPlayer2Pin);  // Активируем кнопку 2 игрока
+  else if (coinCount >= 2) {
+    activateAllRelaysAndLeds();
   }
+
+  timerStart = millis();  // Запускаем таймер на 5 минут
+}
+
+// Активация всех реле и светодиодов
+void activateAllRelaysAndLeds() {
+  digitalWrite(relayCoinPin, LOW);    // Включаем реле для монетки
+  digitalWrite(ledCoinPin, HIGH);     // Включаем светодиод для монетки
+
+  digitalWrite(relayPlayer1Pin, LOW); // Включаем реле для 1 игрока
+  digitalWrite(ledPlayer1Pin, HIGH);  // Включаем светодиод для 1 игрока
+
+  digitalWrite(relayPlayer2Pin, LOW); // Включаем реле для 2 игрока
+  digitalWrite(ledPlayer2Pin, HIGH);  // Включаем светодиод для 2 игрока
 }
 
 // Активация реле и светодиодов
@@ -82,6 +123,7 @@ void activateRelayAndLed(int relayPin, int ledPin) {
 
 // Сброс системы в начальное состояние
 void resetSystem() {
+  gameActive = false;
   coinCount = 0;
   digitalWrite(relayPlayer1Pin, HIGH);
   digitalWrite(relayPlayer2Pin, HIGH);
@@ -89,4 +131,48 @@ void resetSystem() {
   digitalWrite(ledCoinPin, LOW);
   digitalWrite(ledPlayer1Pin, LOW);
   digitalWrite(ledPlayer2Pin, LOW);
+}
+
+// Моргание светодиода монетки три раза
+void blinkCoinThreeTimes() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledCoinPin, HIGH);
+    delay(70);
+    digitalWrite(ledCoinPin, LOW);
+    delay(100);
+  }
+  digitalWrite(ledCoinPin, HIGH); // Оставляем свет включенным
+}
+
+void breatheLed(int pin) {
+  unsigned long currentTime = millis();
+  if (currentTime - lastBreathUpdate > 8) {
+    lastBreathUpdate = currentTime;
+    if (breathUp) {
+      breathValue++;
+      if (breathValue >= 255) {
+        breathValue = 255;
+        breathUp = false;
+      }
+    } else {
+      breathValue--;
+      if (breathValue <= 0) {
+        breathValue = 0;
+        breathUp = true;
+      }
+    }
+    analogWrite(pin, breathValue);  // Регулируем яркость светодиода
+  }
+}
+
+// Мигание светодиода монетки с увеличивающейся частотой
+void blinkCoinWithIncreasingSpeed(unsigned long currentTime) {
+  unsigned long timeRemaining = activeTime - (currentTime - timerStart);
+  unsigned long blinkInterval = map(timeRemaining, 0, warningTime, 50, 500);
+
+  if (currentTime - blinkStartTime > blinkInterval) {
+    blinkStartTime = currentTime;
+    blinkPhase = !blinkPhase;
+    digitalWrite(ledCoinPin, blinkPhase ? HIGH : LOW);
+  }
 }
